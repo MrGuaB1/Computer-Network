541e7784a3f4488a70f7c3543d2a2fae4507a2d0
cmh <cmh1447283266@gmail.com>
2023-10-16 23:22:54 +0800

    finish lab1

--- Lab1/code/main.cpp
+++ Lab1/code/main.cpp
@@ -1,21 +1,3 @@
-//#include"client.h"
-//#include"server.h"
-//
-//int main() {
-//	cout << "Please select which you want to be:\n0. Server\t1. Client\n";
-//	int choose_server_or_client;
-//	cin >> choose_server_or_client;
-//	cin.get();
-//	if (choose_server_or_client == 0)
-//		serverMain();
-//	else if (choose_server_or_client == 1)
-//		clientMain();
-//	else
-//		cout << "Error! You must be Server or Client!";
-//	return 0;
-//}
-//
-
 #include <iostream>
 #include "client.h"
 #include "server.h"
